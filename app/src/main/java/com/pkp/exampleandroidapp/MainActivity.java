054e26381d876a530cbec63d3bd8d9f20fcb296d
kpushpakumara89 <kpushpakumarajd@gmail.com>
2022-11-14 22:32:29 +0530

    Linear Layout xml

--- app/src/main/java/com/pkp/exampleandroidapp/MainActivity.java
+++ app/src/main/java/com/pkp/exampleandroidapp/MainActivity.java
@@ -9,29 +9,9 @@ import android.widget.Button;
 import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
-    Button btn;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
-        btn = findViewById(R.id.btnMain);
-
-        Intent intent = new Intent(Intent.ACTION_SEND);
-        intent.setData(Uri.parse("mailto:"));
-        intent.putExtra(Intent.EXTRA_EMAIL,new String[]{"pradeep@gmail.com"});
-        intent.putExtra(Intent.EXTRA_SUBJECT,"This is our test Subject");
-        intent.putExtra(Intent.EXTRA_TEXT,"This is our test email body");
-        intent.setType("massage/rfc88");
-        Intent chooser = Intent.createChooser(intent, "Send email text app");
-
-
-        //set listener to the button
-        btn.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                startActivity(chooser);
-            }
-        });
     }
 }
