a0c1b7abad7f731660412606077b38298aa44c73
kpushpakumara89 <kpushpakumarajd@gmail.com>
2022-11-30 13:04:21 +0530

    Toast initial

--- app/src/main/java/com/pkp/exampleandroidapp/MainActivity.java
+++ app/src/main/java/com/pkp/exampleandroidapp/MainActivity.java
@@ -7,11 +7,17 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void showToast(View view) {
+        Toast toast = Toast.makeText(getApplicationContext(), "Hello this is a toast", Toast.LENGTH_LONG);
+        toast.show();
     }
 }
